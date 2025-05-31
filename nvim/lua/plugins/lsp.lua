7e982e22b789460e87a65b720047c3367e77041e
blasted-tiara <enver.podgorcevic@gmail.com>
2025-05-31 12:07:43 +0200

    separate plugin config into separate files

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        opts = {}
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("lspconfig").lua_ls.setup({
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = "LuaJIT",
+                            path = vim.split(package.path, ";"),
+                        },
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true),
+                            checkThirdParty = false,
+                        },
+                        telemetry = { enable = false },
+                    },
+                },
+            })
+        end,
+    },
+}

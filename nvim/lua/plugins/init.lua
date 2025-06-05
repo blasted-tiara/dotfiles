10d8922aeda9ea0d95f3e71c4a9b07f18a47f17a
blasted-tiara <enver.podgorcevic@gmail.com>
2025-06-06 00:13:09 +0200

    Add telescope for all kinds of search

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -14,6 +14,23 @@ return {
         end,
     },
     {
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+            vim.keymap.set('n', '<leader>fp', function()
+                require('telescope.builtin').planets({
+                    show_pluto = true,
+                    show_moon = true,
+                })
+            end, { desc = 'Telescope help tags' })
+        end
+    },
+    {
         "nvim-tree/nvim-tree.lua",
         dependencies = {
             "nvim-tree/nvim-web-devicons",
